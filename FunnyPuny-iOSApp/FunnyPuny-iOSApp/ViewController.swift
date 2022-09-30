4982568e3f45e1a641d3ca197e0ad157fa3f63a5
Zlata Guseva <gusevazlata96@gmail.com>
2022-09-30 16:36:04 +0400

    Create project

---
+++ FunnyPuny-iOSApp/FunnyPuny-iOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunnyPuny-iOSApp
+//
+//  Created by Zlata Guseva on 30/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
