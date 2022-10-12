1060919e9119f95d0e453a34077b57f428a7d24c
Zlata Guseva <gusevazlata96@gmail.com>
2022-10-12 18:36:39 +0400

    Add UIColor extension

---
+++ FunnyPuny-iOSApp/FunnyPuny-iOSApp/Presentation/Common /Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  FunnyPuny-iOSApp
+//
+//  Created by Zlata Guseva on 12/10/2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primaryText = UIColor(named: "primaryText")
+}
