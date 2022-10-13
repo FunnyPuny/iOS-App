288e6cfc1c34e93d10db1604c20d77e3e76e6f1a
Zlata Guseva <gusevazlata96@gmail.com>
2022-10-13 18:30:37 +0400

    Add addButton in the middle of tabBar

--- FunnyPuny-iOSApp/FunnyPuny-iOSApp/Presentation/TabBar/TabBarController.swift
+++ FunnyPuny-iOSApp/FunnyPuny-iOSApp/Presentation/TabBar/TabBarController.swift
@@ -1,18 +1,30 @@
-//
-//  TabBarController.swift
-//  FunnyPuny-iOSApp
-//
-//  Created by Zlata Guseva on 12/10/2022.
-//
+// TabBarController.swift
+// Created by Zlata Guseva on 12.10.2022.
 
 import UIKit
 
 class TabBarController: UITabBarController {
+    lazy var addButton: UIButton = {
+        let button = UIButton()
+        var configuration = UIButton.Configuration.filled()
+        configuration.image = .init(systemName: "plus.app")
+        configuration.preferredSymbolConfigurationForImage = .init(weight: .bold)
+        configuration.baseBackgroundColor = .mainBackGround
+        configuration.baseForegroundColor = .gray
+        button.configuration = configuration
+        button.addTarget(self, action: #selector(addButtonPressed), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupStyleTabBar()
         setupTabBarItems()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        addButton.frame = CGRect(x: tabBar.center.x - 32, y: view.bounds.height - 96, width: 64, height: 64)
     }
 
     func setupStyleTabBar() {
@@ -21,21 +33,30 @@ class TabBarController: UITabBarController {
     }
 
     func setupTabBarItems() {
+        view.insertSubview(addButton, aboveSubview: tabBar)
         let firstViewController = HomeViewController()
         firstViewController.tabBarItem = UITabBarItem(
-            title: "Add",
-            image: .add,
+            title: "Habits",
+            image: .init(systemName: "heart.text.square"),
             tag: 0
         )
-        
-        let secondViewController = UIViewController()
-        secondViewController.tabBarItem = UITabBarItem(
+
+        let thirdViewController = UIViewController()
+        thirdViewController.tabBarItem = UITabBarItem(
             title: "Profile",
             image: .init(systemName: "book.closed"),
             tag: 1
         )
 
-        viewControllers = [firstViewController, secondViewController]
+        viewControllers = [firstViewController, thirdViewController]
     }
 
+    @objc
+    private func addButtonPressed() {
+        let addNewHabitViewController = AddNewHabitViewController()
+        if let sheet = addNewHabitViewController.sheetPresentationController {
+            sheet.detents = [.large()]
+        }
+        present(addNewHabitViewController, animated: true, completion: nil)
+    }
 }
