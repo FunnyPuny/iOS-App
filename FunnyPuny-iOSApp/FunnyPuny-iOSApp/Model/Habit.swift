27f5c3e1206116332cc3a0b002ab7e3627465ae8
Zlata Guseva <gusevazlata96@gmail.com>
2022-10-14 16:23:55 +0400

    Add Habit model

---
+++ FunnyPuny-iOSApp/FunnyPuny-iOSApp/Model/Habit.swift
@@ -0,0 +1,13 @@
+// Habit.swift
+// Created by Zlata Guseva on 14.10.2022.
+
+import Foundation
+
+struct Habit {
+    var name: String
+    var period: Period
+}
+
+enum Period {
+    case everyday
+}
