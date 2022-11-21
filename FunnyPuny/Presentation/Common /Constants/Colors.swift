52c9778d59974765b7dc4c5bc0965c63d3da2d9e
Zlata Guseva <gusevazlata96@gmail.com>
2022-11-22 01:43:04 +0400

    Add Colors Sets

--- FunnyPuny/Presentation/Common /Constants/Colors.swift
+++ FunnyPuny/Presentation/Common /Constants/Colors.swift
@@ -18,4 +18,48 @@ extension UIColor {
     static let pinkLight = UIColor(named: "pinkLight")
     /// Grey dark
     static let greyDark = UIColor(named: "greyDark")
+
+    // MARK: Background
+    /// Vivid Pink
+    static let backgroundAccent = UIColor(named: "backgroundAccent")
+    /// Grey light
+    static let backgroundBoarder = UIColor(named: "backgroundBoarder")
+    /// White
+    static let backgroundGlobe = UIColor(named: "backgroundGlobe")
+    /// Pink light
+    static let backgroundIsland = UIColor(named: "backgroundIsland")
+    /// Grey dark
+    static let backgroundOther = UIColor(named: "backgroundOther")
+
+    // MARK: Texts
+    /// Vivid Pink
+    static let textAccent = UIColor(named: "textAccent")
+    /// Black
+    static let textPrimary = UIColor(named: "textPrimary")
+    /// White
+    static let textPrimaryInverse = UIColor(named: "textPrimaryInverse")
+    /// Grey dark
+    static let textSecondary = UIColor(named: "textSecondary")
+    /// Grey light
+    static let textTertiary = UIColor(named: "textTertiary")
+
+    // MARK: Buttons
+    /// Vivid Pink
+    static let buttonActive = UIColor(named: "buttonActive")
+    /// Grey light
+    static let buttonInactive = UIColor(named: "buttonInactive")
+
+    // MARK: Icons
+    /// Vivid Pink
+    static let iconsActive = UIColor(named: "iconsActive")
+    /// Black
+    static let iconsDefault = UIColor(named: "iconsDefault")
+    /// Main grey
+    static let iconsInactive = UIColor(named: "iconsInactive")
+    /// Grey dark
+    static let iconsOther = UIColor(named: "iconsOther")
+    
+    // MARK: Other
+    /// Grey dark
+    static let inputDefault = UIColor(named: "inputDefault")
 }
