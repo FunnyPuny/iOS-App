77ae49c81c5c1b8a00b7083e58f09dd12d5257ef
Bogdan Bystritskiy <bystritskiyb@gmail.com>
2022-12-12 21:52:07 +0400

    Create ci_pre_xcodebuild.sh

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,19 @@
+# find the directory where the executable script file is located
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# go to it
+cd "$DIR"
+
+# install SwiftGen, if needed
+brew install swiftgen
+
+# create SwiftGen generated files and perform initial initialization
+function mkdir_touch {
+  mkdir -p "$(dirname "$1")"
+  command touch "$1"
+}
+
+mkdir_touch FunnyPuny/Generated/Assets.swift
+mkdir_touch FunnyPuny/Generated/Texts.swift
+
+swiftgen config run
